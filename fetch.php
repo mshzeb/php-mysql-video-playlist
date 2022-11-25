86115cefc601b59f3a479b40e5683671e9534060
mshzeb <mshzeb@gmail.com>
2022-11-25 17:36:37 +0500

    Add Function Get Data From Last Hour

--- fetch.php
+++ fetch.php
@@ -8,11 +8,12 @@ if(!$con) {
     die("Connection Falied" . mysqli_connect_error());
 }
 //echo "Connected Successfully";
+// Src: https://www.youtube.com/watch?v=ygzfmRjVaV0
 
 if(isset($_POST['request'])) {
     $request = $_POST['request'];
 
-    //$q = "SELECT * FROM tbl_video WHERE "
+    //$q = "SELECT * FROM tbl_video WHERE name"
 }
 
 ?>
--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $con = mysqli_connect("localhost", "root", "", "dbnskhost");
 
 // Check connection
 if(!$con) {
-    die("Connection Falied" . mysqli_connect_error());
+    die("Connection Failed" . mysqli_connect_error());
 }
 //echo "Connected Successfully";
 
@@ -25,11 +25,17 @@ if(!$con) {
 <body>
     <h3 class="heading">Video Playlist Web App</h3>
     <div style="text-align: center;">
-        <form method="GET">
+        <form method="POST">
             <select name="filterChoice" id="selDuration">
-                <option value="0">-- Select --</option>
-                <option value="1">Last Hour</option>
+                <option value="0">-- Select TimeFrame--</option>
+                <option value="1">Last 1 Hour</option>
+                <option value="2">Last 2 Hour</option>
+                <option value="3">Last 4 Hour</option>
+                <option value="4">This Week</option>
+                <option value="5">This Month</option>
+                
             </select>
+            <input type="submit" name="submit" value="Get Data" />
         </form>
     </div>
 
@@ -41,6 +47,8 @@ if(!$con) {
                     <?php
 
                         $getAllVids_query = "SELECT * FROM tbl_video";
+                        //$query = mysqli_query($con, $getAllVids_query);
+                        global $query, $sql1;
                         $query = mysqli_query($con, $getAllVids_query);
                         
                         // (B1) GET VIDEO FILES FROM GALLERY FOLDER
@@ -67,8 +75,57 @@ if(!$con) {
                             // SELECT * FROM `tbl_video` WHERE `date_created` > DATE_SUB(NOW(), INTERVAL 12 HOUR) ORDER BY DAY(`date_created`);
                             // Src: https://www.w3schools.com/sql/func_mysql_date_sub.asp 
 
+// *******************************************************
 
-                            while($row = mysqli_fetch_array($query)) {
+// The most important link
+// Developing the functionality of filter record based on dates using PHP Part 5
+// By How to Make Tut's <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+// Src: https://www.youtube.com/watch?v=65dZ_rlLk1c
+
+                            if(!isset($_POST['filterChoice'])) {
+                                //$mainQuery = "SELECT * FROM tbl_video";
+                                //getData($mainQuery);
+                                //getData($query);
+                                //echo "<script>alert('No Data Selected')</script>";
+                                $sql1 = "SELECT * FROM tbl_video WHERE date_created > DATE_SUB(NOW(), INTERVAL 1 HOUR)";
+                                getData($sql1);
+                                        
+                            } else {
+                                switch($_POST['filterChoice']) {
+                                    case "1":
+                                        //Last 7 days
+                                        $sql1 = "SELECT * FROM tbl_video WHERE date_created > DATE_SUB(NOW(), INTERVAL 15 DAY)";
+                                        getData($sql1);
+                                        break;
+                                    default:
+                                        // Show last 7 days data
+                                        $sql1 = "SELECT * FROM tbl_video WHERE date_created > DATE_SUB(NOW(), INTERVAL 1 HOUR)";
+                                        getData($sql1);
+                                }
+                            }
+                            ?>
+<?php
+// *******************************************************
+
+                            function getData($sql) {
+                                // Create Connection
+                                $con = mysqli_connect("localhost", "root", "", "dbnskhost");
+
+                                // Check connection
+                                if(!$con) {
+                                    die("Connection Failed" . mysqli_connect_error());
+                                }
+                                //echo "Connected Successfully";
+
+                                //$sql = "";
+                                //echo "SQL: "+$sql;
+                                $data = mysqli_query($con, $sql) or die('error');
+                                if(mysqli_num_rows($data) > 0) {
+                                    
+
+// *******************************************************
+                            //while($row = mysqli_fetch_array($query)) {
+                            while($row = mysqli_fetch_array($data)) {
                                 $vidName = basename($row['name']);
                                 $caption = substr($vidName, 0, strrpos($vidName, "."));
                                 //echo $caption;
@@ -81,6 +138,10 @@ if(!$con) {
                                 </div>
 
                             <?php  } ?>
+
+                            <?php 
+                            } 
+                        } ?>
         </div>
                     <?php
                         /*} else {
@@ -152,28 +213,28 @@ if(!$con) {
         };
     });
 
-    $(document).ready(function(){
-        $("#selDuration").change(function(){ 
-            //alert("Select Clicked.");
-            var value = $(this).val();
-            //alert(value);
+    // $(document).ready(function(){
+    //     $("#selDuration").change(function(){ 
+    //         //alert("Select Clicked.");
+    //         var value = $(this).val();
+    //         //alert(value);
 
-            $.ajax({
-                url: "fetch.php",
-                type: "POST",
-                data: 'request=' + value;
-                beforeSend: function() {
-                    $(".video-list").html("<span>Working...</span>");
-                },
-                success: function() {
-                    $("#vid-list").html(data);
-                }
-                // success: function(data) {
-                //     console.log(data);
-                // }
-            });
-        });
-    });
+    //         $.ajax({
+    //             url: "fetch.php",
+    //             type: "POST",
+    //             data: 'request=' + value;
+    //             beforeSend: function() {
+    //                 $(".video-list").html("<span>Working...</span>");
+    //             },
+    //             success: function() {
+    //                 $("#vid-list").html(data);
+    //             }
+    //             // success: function(data) {
+    //             //     console.log(data);
+    //             // }
+    //         });
+    //     });
+    // });
 
 </script>
 </body>
