26a0438d04fa6ad2c5dc644503cc25857756c66a
mshzeb <mshzeb@gmail.com>
2022-11-25 18:23:30 +0500

    Change SQL file in db directory

--- db/dbnskhost.sql
+++ db/dbnskhost.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 25, 2022 at 10:08 AM
+-- Generation Time: Nov 25, 2022 at 02:19 PM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -23,33 +23,13 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_users`
---
-
-CREATE TABLE IF NOT EXISTS `tbl_users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
-
---
--- Dumping data for table `tbl_users`
---
-
-INSERT INTO `tbl_users` (`id`, `username`) VALUES
-(1, 'Talha'),
-(2, 'Mustafa');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `tbl_video`
 --
 
 CREATE TABLE IF NOT EXISTS `tbl_video` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
-  `user_id` int(11) NOT NULL,
+  `user_name` varchar(50) NOT NULL,
   `date_created` datetime NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
@@ -58,12 +38,12 @@ CREATE TABLE IF NOT EXISTS `tbl_video` (
 -- Dumping data for table `tbl_video`
 --
 
-INSERT INTO `tbl_video` (`id`, `name`, `user_id`, `date_created`) VALUES
-(1, '2022-11-07 18-00-06.mp4', 0, '2022-11-07 18:00:06'),
-(2, '2022-11-10 21-47-28.mp4', 0, '2022-11-10 21:47:28'),
-(3, '2022-11-10 21-48-38.mp4', 0, '2022-11-10 21:48:38'),
-(4, '2022-11-11 11-25-22.mp4', 0, '2022-11-25 11:27:18'),
-(5, '2022-11-10 22-32-58.mp4', 0, '2022-11-25 11:57:58');
+INSERT INTO `tbl_video` (`id`, `name`, `user_name`, `date_created`) VALUES
+(1, '2022-11-07 18-00-06.mp4', 'Talha', '2022-11-07 18:00:06'),
+(2, '2022-11-10 21-47-28.mp4', 'AbuSalha', '2022-11-10 21:47:28'),
+(3, '2022-11-10 21-48-38.mp4', 'mustafa', '2022-11-10 21:48:38'),
+(4, '2022-11-11 11-25-22.mp4', 'Tooba', '2022-11-25 11:27:18'),
+(5, '2022-11-10 22-32-58.mp4', 'Hassan', '2022-11-25 11:57:58');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
