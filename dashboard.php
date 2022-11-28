1446c4e9429d7042769aa24d761a753e998131a6
mshzeb <mshzeb@gmail.com>
2022-11-28 18:22:32 +0500

    Code Cleanup

--- dashboard.php
+++ dashboard.php
@@ -25,7 +25,6 @@ date_default_timezone_set("Asia/Karachi");
 <body>
     <h3 class="heading">Video Playlist Web App</h3>
 
-
     <div class="container">
 
         <div class="form-inline text-center">
@@ -68,36 +67,13 @@ date_default_timezone_set("Asia/Karachi");
                 global $query, $sql1;
                 $query = mysqli_query($con, $getAllVids_query);
                 
-                // (B1) GET VIDEO FILES FROM GALLERY FOLDER
-                //$dir = __DIR__ . DIRECTORY_SEPARATOR . "uploads" . DIRECTORY_SEPARATOR;
-                //$vid = glob("$dir*.{webm,mp4,ogg}", GLOB_BRACE);
 
-                // (B2) OUTPUT VIDEOS
-            // if (count($vid) > 0) { ?>
+            ?>
 
                     
                     <?php
 
-                    //Get the first video from the database.
-                    //$row1 = mysqli_fetch_assoc($query);
-                    //$firstRow = $row1;
-                    //echo $firstRow['name'];
-
-                        // Seek to First row in the database before looping through the video files
-                    //mysqli_data_seek($query, 0);
-
-                    // Important Query
-                    // SELECT * FROM `tbl_video` WHERE `date_created` < date_sub(now(), INTERVAL 1 HOUR)
-                    // SELECT * FROM `tbl_video` WHERE `date_created` > now() - INTERVAL 1 HOUR;
-                    // SELECT * FROM `tbl_video` WHERE `date_created` > DATE_SUB(NOW(), INTERVAL 12 HOUR) ORDER BY DAY(`date_created`);
-                    // Src: https://www.w3schools.com/sql/func_mysql_date_sub.asp 
-
 // *******************************************************
-
-// The most important link
-// Developing the functionality of filter record based on dates using PHP Part 5
-// By How to Make Tut's <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-// Src: https://www.youtube.com/watch?v=65dZ_rlLk1c
 
                     // The following if code loads whenever the user refresh or load the page for the first time.
                     if(!isset($_POST['filterChoice'])) {
@@ -115,15 +91,8 @@ date_default_timezone_set("Asia/Karachi");
 
                                     //date_default_timezone_set("Asia/Karachi");
                                     $to_time = date("Y-m-d H-i-s");
-                                    //echo $dateNow;
-
                                     $from_time = date("Y-m-d H-i-s", strtotime('-1 hour'));
-                                    //echo '<br />One hour ago: '.$final_time;
-                                    //echo '<br /> Type: ' . gettype($final_time);
 
-                                    //$sql1 = "SELECT * FROM tbl_video WHERE name > DATE_SUB(NOW(), INTERVAL 1 MONTH) AND user_name='$_POST[filterUser]' ";
-                                    //$sql1 = "SELECT * FROM tbl_video WHERE name < '$final_time' AND user_name='$_POST[filterUser]' ";
-                                    //$sql1 = "SELECT * FROM tbl_video WHERE name like '%$final_time%' AND user_name='$_POST[filterUser]' ";
                                     $sql1 = "SELECT * FROM tbl_video WHERE (name BETWEEN '$from_time' AND '$to_time') AND user_name='$_POST[filterUser]' ";
                                     //echo $sql1;
                                     getData($sql1);
@@ -132,17 +101,8 @@ date_default_timezone_set("Asia/Karachi");
                                     //Last 2 hours
                                     
                                     $to_time = date("Y-m-d H-i-s");
-                                    //echo $dateNow;
-
-                                    //$from_time = strtotime($dateNow);
-                                    //$final_time = date("Y-m-d H-i-s", strtotime('-2 hour'));
                                     $from_time = date("Y-m-d H-i-s", strtotime('-2 hour'));
-                                    
-                                    // echo '<br />Two hour ago: '.$final_time;
-                                    //echo '<br /> Type: ' . gettype($final_time);
-                                    
-                                    //$sql1 = "SELECT * FROM tbl_video WHERE date_created > DATE_SUB(NOW(), INTERVAL 2 HOUR)";
-                                    //$sql1 = "SELECT * FROM tbl_video WHERE name like '%$final_time%' AND user_name='$_POST[filterUser]' ";
+
                                     $sql1 = "SELECT * FROM tbl_video WHERE (name BETWEEN '$from_time' AND '$to_time') AND user_name='$_POST[filterUser]' ";
                                     //echo '<br />' .$sql1;
                                     getData($sql1);
@@ -151,19 +111,8 @@ date_default_timezone_set("Asia/Karachi");
                                     //Last 1 day
 
                                     $to_time = date("Y-m-d H-i-s");
-                                    //echo "To Time: " . $to_time;
-
                                     $from_time = date("Y-m-d H-i-s", strtotime('-1 day'));
-                                    //echo "From Time: " . $from_time;
-                                    
-                                    //$final_time = date("Y-m-d H-i-s", strtotime('-1 day'));
-                                    //$to_time = date("Y-m-d H-i-s", strtotime('-2 day'));
-                                    
-                                    // echo '<br />Two hour ago: '.$final_time;
-                                    //echo '<br /> Type: ' . gettype($final_time);
 
-                                    //$sql1 = "SELECT * FROM tbl_video WHERE date_created > DATE_SUB(NOW(), INTERVAL 1 DAY)";
-                                    //$sql1 = "SELECT * FROM tbl_video WHERE name < '$final_time' AND user_name='$_POST[filterUser]' ";
                                     $sql1 = "SELECT * FROM tbl_video WHERE (name BETWEEN '$from_time' AND '$to_time') AND user_name='$_POST[filterUser]' ";
                                     getData($sql1);
                                     break;
@@ -171,15 +120,8 @@ date_default_timezone_set("Asia/Karachi");
                                     //Last 7 days
 
                                     $to_time = date("Y-m-d H-i-s");
-                                    //echo $dateNow;
-                                    
                                     $from_time = date("Y-m-d H-i-s", strtotime('-7 day'));
-                                    
-                                    // echo '<br />Two hour ago: '.$final_time;
-                                    //echo '<br /> Type: ' . gettype($final_time);
 
-                                    //$sql1 = "SELECT * FROM tbl_video WHERE name < '$final_time' AND user_name='$_POST[filterUser]' ";
-                                    //$sql1 = "SELECT * FROM tbl_video WHERE name like '%$final_time%' AND user_name='$_POST[filterUser]' ";
                                     $sql1 = "SELECT * FROM tbl_video WHERE (name BETWEEN '$from_time' AND '$to_time') AND user_name='$_POST[filterUser]' ";
                                     getData($sql1);
                                     break;
@@ -187,15 +129,8 @@ date_default_timezone_set("Asia/Karachi");
                                     //Last 14 days
 
                                     $to_time = date("Y-m-d H-i-s");
-                                    //echo $dateNow;
-
                                     $from_time = date("Y-m-d H-i-s", strtotime('-14 day'));
-                                    // echo '<br />Two hour ago: '.$final_time;
-                                    //echo '<br /> Type: ' . gettype($final_time);
-                                    
-                                    //$sql1 = "SELECT * FROM tbl_video WHERE date_created > DATE_SUB(NOW(), INTERVAL 2 WEEK)";
-                                    //$sql1 = "SELECT * FROM tbl_video WHERE name < '$final_time' AND user_name='$_POST[filterUser]' ";
-                                    //$sql1 = "SELECT * FROM tbl_video WHERE name like '%$final_time%' AND user_name='$_POST[filterUser]' ";
+
                                     $sql1 = "SELECT * FROM tbl_video WHERE (name BETWEEN '$from_time' AND '$to_time') AND user_name='$_POST[filterUser]' ";
                                     getData($sql1);
                                     break;
@@ -203,16 +138,8 @@ date_default_timezone_set("Asia/Karachi");
                                     //Last 30 days
 
                                     $to_time = date("Y-m-d H-i-s");
-                                    //echo $dateNow;
-
                                     $from_time = date("Y-m-d H-i-s", strtotime('-1 month'));
-                                    // echo '<br />Two hour ago: '.$final_time;
-                                    //echo '<br /> Type: ' . gettype($final_time);
-                                    
-                                    //$sql1 = "SELECT * FROM tbl_video WHERE date_created > DATE_SUB(NOW(), INTERVAL 1 MONTH)";
-                                    //$sql1 = "SELECT * FROM tbl_video WHERE name < '$final_time' AND user_name='$_POST[filterUser]' ";
-                                    
-                                    //echo $sql1 = "SELECT * FROM tbl_video WHERE name like '%$final_time%' AND user_name='$_POST[filterUser]' ";
+
                                     $sql1 = "SELECT * FROM tbl_video WHERE (name BETWEEN '$from_time' AND '$to_time') AND user_name='$_POST[filterUser]' ";
                                     getData($sql1);
                                     break;
@@ -226,37 +153,21 @@ date_default_timezone_set("Asia/Karachi");
                 ?>
 <?php
 // *******************************************************
-
             function getData($sql) {
-                // // Create Connection
-                // $con = mysqli_connect("localhost", "root", "", "dbnskhost");
-
-                // // Check connection
-                // if(!$con) {
-                //     die("Connection Failed" . mysqli_connect_error());
-                // }
-                // //echo "Connected Successfully";
 
                 require('./dbconfig.php');
 
-                //$sql = "";
-                //echo "SQL: "+$sql;
                 $data = mysqli_query($con, $sql) or die('error: ' . mysqli_error($con));
-                if(mysqli_num_rows($data) > 0) {
-                                    
+                if(mysqli_num_rows($data) > 0) {                            
 
 // *******************************************************
-                //while($row = mysqli_fetch_array($query)) {
                 while($row = mysqli_fetch_array($data)) {
                     $vidName = basename($row['name']);
                     $caption = substr($vidName, 0, strrpos($vidName, "."));
-                    //echo $caption;
-                    //echo "<script>alert('vid name'".$row[0]['name'].")</script>";
-                ?>
+?>
 
                     <!-- Load videos based on query on line 84 on page load -->
                     <div class="vid" id="vidlist"> 
-                        <!-- <div> <?php //echo 'User: '.$row["user_name"]; ?> </div> -->
                         <video src="<?php echo 'uploads/'.$row["name"]; ?>"></video>
                         <div class="title"><?php echo $caption; ?></div>
                     </div>
@@ -268,46 +179,15 @@ date_default_timezone_set("Asia/Karachi");
             
         } ?>
         </div>
-                    <?php
-                        /*} else {
-                            echo "No Video Files Found.";
-                        }*/
-                    ?>
-
-
-        <!-- </div> -->
-        <!-- (B) VIDEO GALLERY ENDS -->
-
-
-        <!--
-        <div class="video-list">
-            <div class="vid active">
-                <video src="uploads/vid1.mp4" muted></video>
-                <h3 class="title">1 Video Title Goes Here</h3>
-            </div>
-            <div class="vid">
-                <video src="uploads/vid2.mp4" muted></video>
-                <h3 class="title">2 Video Title Goes Here</h3>
-            </div>
-            <div class="vid">
-                <video src="uploads/vid3.mp4" muted></video>
-                <h3 class="title">3 Video Title Goes Here</h3>
-            </div>
-            <div class="vid">
-                <video src="uploads/vid4.mp4" muted></video>
-                <h3 class="title">4 Video Title Goes Here</h3>
-            </div>
-        </div>
-        -->
 
         <?php
 
+            //EXTRA CODE
             $getAllVids_query1 = "SELECT * FROM tbl_video";
             $query1 = mysqli_query($con, $getAllVids_query1);
 
             $row1 = mysqli_fetch_assoc($query1);
             $firstRow = $row1;
-            //echo $firstRow['name'];
         ?>
 
         <div class="main-video">
@@ -322,98 +202,30 @@ date_default_timezone_set("Asia/Karachi");
 
 <script>
 
-    $(document).ready(function(){
+$(document).ready(function(){
 
-        // $(".vid").click(function() {
-        //     //$(this).addClass("active");
-        //     $(this).classList.add("active");
+    $(document).on("click","div.video-list .vid",function(){
+        $(this).addClass('active');
+        $("div.video-list .vid").not(this).removeClass("active");
 
-        //     $(".vid").not(this).removeClass("active");
-        // });
+        let mainVideo = document.querySelector('.main-video video');
+        let listVideo = document.querySelectorAll('.video-list .vid');
+        let title = document.querySelector('.main-video .title');
 
-        $(document).on("click","div.video-list .vid",function(){
-            $(this).addClass('active');
-            $("div.video-list .vid").not(this).removeClass("active");
-
-            let mainVideo = document.querySelector('.main-video video');
-            let listVideo = document.querySelectorAll('.video-list .vid');
-            //console.log(listVideo);
-            let title = document.querySelector('.main-video .title');
-
-            listVideo.forEach(video => {
-                if(video.classList.contains('active')) {
-                    let src = video.children[0].getAttribute('src');
-                    mainVideo.src = src;
-                    let text = video.children[1].innerHTML;
-                    title.innerHTML = text;
-                };
-            });
+        listVideo.forEach(video => {
+            if(video.classList.contains('active')) {
+                let src = video.children[0].getAttribute('src');
+                mainVideo.src = src;
+                let text = video.children[1].innerHTML;
+                title.innerHTML = text;
+            };
         });
+    });
 
-        
-
-        // Src: https://stackoverflow.com/questions/2246227/keep-values-selected-after-form-submission
-        document.getElementById('fUser').value = "<?php echo $_POST['filterUser']; ?>";
-        document.getElementById('selDuration').value = "<?php echo $_POST['filterChoice']; ?>";
-
-        // let listVideo = document.querySelectorAll('.video-list .vid');
-        // //let listVideo = document.querySelectorAll('.container .video-list .vid');
-        // let mainVideo = document.querySelector('.main-video video');
-        // let title = document.querySelector('.main-video .title');
-
-        // //console.log(listVideo);
-
-        // listVideo.forEach(video => {
-        //     video.onclick = () => {
-        //         listVideo.forEach(vid => vid.classList.remove('active'));
-        //         video.classList.add('active');
-        //         if(video.classList.contains('active')) {
-        //             let src = video.children[0].getAttribute('src');
-        //             mainVideo.src = src;
-        //             let text = video.children[1].innerHTML;
-        //             title.innerHTML = text;
-        //         };
-        //     };
-        // });
-
-        // // Get all video elements
-        // var container = document.getElementById("mVidList");
-        // //console.log(container);
-
-        // // Get all video inside video-list class
-        // var vid = container.getElementsByClassName("vid");
-
-        // // Loop through the vid elements and add the active class to the current/clicked vid element
-        // for(var i = 0; i < vid.length; i++) {
-        //     vid[i].addEventListener("click", function(){
-        //         var current = document.getElementsByClassName("active");
-
-        //         current[0].className = current[0].className.replace(" active", "");
-        //         this.className += " active";
-
-        //         // If there is no active class
-        //         // if(current.length > 0) {
-        //         //     current[0].className = current[0].className.replace(" active", "");
-        //         // }
-
-        //         // Add the active class to the current/clicked button
-        //         //this.className += " active";
-        //     });
-        // }
-        
-            // $.ajax({
-            //     url: "fetch.php",
-            //     beforeSend: function() {
-            //         $(".video-list").html("<span>Working...</span>");
-            //     },
-            //     success: function(data) {
-            //         $(".video-list").html(data);
-            //     }
-            //     // success: function(data) {
-            //     //     console.log(data);
-            //     // }
-            // });
-        });
+    document.getElementById('fUser').value = "<?php echo $_POST['filterUser']; ?>";
+    document.getElementById('selDuration').value = "<?php echo $_POST['filterChoice']; ?>";
+    
+});
 
 </script>
 </body>
--- db/dbnskhost.sql
+++ db/dbnskhost.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 25, 2022 at 02:19 PM
+-- Generation Time: Nov 28, 2022 at 02:03 PM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -23,27 +23,69 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbl_admin`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_admin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  `pass` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `tbl_admin`
+--
+
+INSERT INTO `tbl_admin` (`id`, `name`, `pass`) VALUES
+(1, 'admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_users`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `tbl_users`
+--
+
+INSERT INTO `tbl_users` (`id`, `name`) VALUES
+(1, 'talhamuzammil'),
+(2, 'mustafatahir');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbl_video`
 --
 
 CREATE TABLE IF NOT EXISTS `tbl_video` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
-  `user_name` varchar(50) NOT NULL,
+  `user_name` varchar(25) NOT NULL,
   `date_created` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
 --
 -- Dumping data for table `tbl_video`
 --
 
 INSERT INTO `tbl_video` (`id`, `name`, `user_name`, `date_created`) VALUES
-(1, '2022-11-07 18-00-06.mp4', 'Talha', '2022-11-07 18:00:06'),
-(2, '2022-11-10 21-47-28.mp4', 'AbuSalha', '2022-11-10 21:47:28'),
-(3, '2022-11-10 21-48-38.mp4', 'mustafa', '2022-11-10 21:48:38'),
-(4, '2022-11-11 11-25-22.mp4', 'Tooba', '2022-11-25 11:27:18'),
-(5, '2022-11-10 22-32-58.mp4', 'Hassan', '2022-11-25 11:57:58');
+(1, '2022-11-07 18-00-06.mp4', 'talhamuzammil', '2022-11-07 18:00:06'),
+(2, '2022-11-10 21-47-28.mp4', 'abusalha', '2022-11-10 21:47:28'),
+(3, '2022-11-10 21-48-38.mp4', 'mustafatahir', '2022-11-10 21:48:38'),
+(4, '2022-11-11 11-25-22.mp4', 'hassan', '2022-11-25 11:27:18'),
+(5, '2022-11-10 22-32-58.mp4', 'abusalha', '2022-11-26 11:57:58'),
+(6, '2022-11-26 21-47-28.mp4', 'talhamuzammil', '2022-11-26 14:47:28'),
+(7, '2022-11-28 17-30-00.mp4', 'hassan', '2022-11-28 17:30:00');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
